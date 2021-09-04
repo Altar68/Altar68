0d6288de5e4fd02023977e27e26d9982aa3c5dce
Altar68 <77123908+Altarplayer@users.noreply.github.com>
2021-09-04 10:33:00 +0200

    TEST 1

---
+++ src/main/java/net/mcreator/moore/item/PlatiniumItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.moore.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.moore.MooreModElements;
+
+@MooreModElements.ModElement.Tag
+public class PlatiniumItem extends MooreModElements.ModElement {
+	@ObjectHolder("moore:platinium")
+	public static final Item block = null;
+	public PlatiniumItem(MooreModElements instance) {
+		super(instance, 3);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).isImmuneToFire().rarity(Rarity.COMMON));
+			setRegistryName("platinium");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
